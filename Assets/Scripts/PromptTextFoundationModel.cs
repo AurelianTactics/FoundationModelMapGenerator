acf3aef2c3c40b46d0d57a7d5959c19148458679
AurelianTactics <jamesdilorenzo@gmail.com>
2023-11-19 19:25:31 -0500

    testing copilot

---
+++ Assets/Scripts/PromptTextFoundationModel.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// This class sends a prompt to a Foundation Model and receives a response.
+/// </summary>
+public class PromptTextFoundationModel : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //connect to the foundation model
+    void ConnectToFoundationModel()
+    {
+		//connect to ChatGPT AI
+        //connect to GPT-3
+        
+	}
+
+    //send a prompt
+    void SendPrompt()
+    { }
+
+
+    //receive a response
+    void ReceiveResponse()
+    { }
+
+    //display the response
+    void DisplayResponse()
+    { }
+}
